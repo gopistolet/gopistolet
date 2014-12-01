affe5b3a451766fb3c35885bbc666cbccad23da2
MathiasB <git@denbeke.be>
2014-12-01 19:24:26 +0100

    Added config_reader.go

---
+++ config_reader.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+)
+
+// DecodeFile is a more generic JSON parser
+func DecodeFile(fileName string, object interface{}) error {
+
+	//Open the config file
+	file, err := os.Open(fileName)
+
+	if err != nil {
+		return errors.New("Could not open file: " + err.Error())
+	}
+
+	jsonParser := json.NewDecoder(file)
+	err = jsonParser.Decode(object)
+
+	if err != nil {
+		return errors.New("Could not parse file: " + err.Error())
+	} else {
+		return nil
+	}
+
+}
